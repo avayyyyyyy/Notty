e9824337872a693621769cc1086f36902721a5f4
avayyyyyyy <jabhi465@gmail.com>
2024-03-26 15:15:00 +0530

    🐞 fix: Changed from Flex to Grid

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,13 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = () => {
   return (
-    <div className="w-screen text-primary px-20 py-5 items-center flex border-b-2 shadow-sm justify-between">
-      <div className="md:min-w-44  cursor-pointer">
-        <Menu />
+    <div className="w-screen grid grid-cols-12  text-primary px-20 py-5 items-center border-b-2 shadow-sm justify-between">
+      <div className="col-span-4">
+        <Menu className="cursor-pointer" />
       </div>
-      <div className="font-bold text-5xl">NOTTY</div>
-      <div className="flex space-x-3 items-center">
-        <div>
+      <div className="font-extrabold md:font-bold col-span-4 text-center text-3xl md:text-4xl  lg:text-5xl">NOTTY</div>
+      <div className="col-span-4 justify-end  flex space-x-3 items-center">
+        <div className="hidden md:block">
           <ModeToggle />
         </div>
         <div className=" text-primary hidden cursor-pointer  md:block ">
